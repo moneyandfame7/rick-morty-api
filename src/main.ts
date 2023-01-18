76de702bcddb1034c040aa77bab12c038a6b5b04
moneyandfame7 <davidoo1234e@gmail.com>
2023-01-18 21:45:50 +0200

    feat: was added dto for page info

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { BackendValidationPipe } from './pipes/validation.pipe'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const PORT = process.env.PORT || 3000
   const app = await NestFactory.create(AppModule)
 
   app.setGlobalPrefix('api')
-  app.useGlobalPipes(new BackendValidationPipe())
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }))
   await app.listen(PORT, () => console.log(` >> 🥶️🚀Server started on http://localhost:${PORT}/api >> `))
 }
 
