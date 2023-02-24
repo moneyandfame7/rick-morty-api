8eed7eab0aa175c0c2e324979fc7ef9197942c41
moneyandfame7 <davidoo1234e@gmail.com>
2023-02-24 19:53:54 +0200

    feat: was implemented custom "GetUser" decorator

--- src/app/controllers/auth/discord.controller.ts
+++ src/app/controllers/auth/discord.controller.ts
@@ -1,16 +1,16 @@
-import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common'
-import type { Request, Response } from 'express'
+import { Controller, Get, Res, UseGuards } from '@nestjs/common'
+import type { Response } from 'express'
 
 import { AuthService } from '@app/services/auth/auth.service'
 import { UserService } from '@app/services/common/user.service'
 import { TokenService } from '@app/services/common/token.service'
+import { EnvironmentConfigService } from '@app/services/common/environment-config.service'
 
 import type { AuthTokens, UserBeforeAuthentication } from '@core/models'
 import { BaseController } from '@core/controllers/auth/base-controller'
 
 import { DiscordAuthGuard } from '@common/guards/auth/discord.guard'
-
-import { EnvironmentConfigService } from '@app/services/common/environment-config.service'
+import { GetUser } from '@common/decorators/user.decorator'
 
 @Controller('/auth/discord')
 export class DiscordController extends BaseController {
@@ -29,8 +29,7 @@ export class DiscordController extends BaseController {
 
   @Get('/redirect')
   @UseGuards(DiscordAuthGuard)
-  public async redirect(@Req() req: Request, @Res({ passthrough: true }) res: Response): Promise<AuthTokens> {
-    const user = req.user as UserBeforeAuthentication
+  public async redirect(@GetUser() user: UserBeforeAuthentication, @Res({ passthrough: true }) res: Response): Promise<AuthTokens> {
     const tokens = await this.socialLogin(user)
     this.setCookies(res, tokens.refresh_token, tokens.access_token)
     return tokens
--- src/app/controllers/auth/google.controller.ts
+++ src/app/controllers/auth/google.controller.ts
@@ -1,16 +1,16 @@
-import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common'
-import type { Request, Response } from 'express'
+import { Controller, Get, Res, UseGuards } from '@nestjs/common'
+import type { Response } from 'express'
 
 import { UserService } from '@app/services/common/user.service'
 import { TokenService } from '@app/services/common/token.service'
 import { AuthService } from '@app/services/auth/auth.service'
+import { EnvironmentConfigService } from '@app/services/common/environment-config.service'
 
 import { BaseController } from '@core/controllers/auth/base-controller'
 import type { AuthTokens, UserBeforeAuthentication } from '@core/models'
 
 import { GoogleAuthGuard } from '@common/guards/auth/google.guard'
-
-import { EnvironmentConfigService } from '@app/services/common/environment-config.service'
+import { GetUser } from '@common/decorators/user.decorator'
 
 @Controller('/auth/google')
 export class GoogleController extends BaseController {
@@ -29,8 +29,7 @@ export class GoogleController extends BaseController {
 
   @Get('/redirect')
   @UseGuards(GoogleAuthGuard)
-  public async redirect(@Req() req: Request, @Res({ passthrough: true }) res: Response): Promise<AuthTokens> {
-    const user = req.user as UserBeforeAuthentication
+  public async redirect(@GetUser() user: UserBeforeAuthentication, @Res({ passthrough: true }) res: Response): Promise<AuthTokens> {
     const tokens = await this.socialLogin(user)
     this.setCookies(res, tokens.refresh_token, tokens.access_token)
     return tokens
