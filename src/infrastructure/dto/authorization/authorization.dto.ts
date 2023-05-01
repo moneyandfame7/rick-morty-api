4ebb666cb95c5a4b5889a59801ce38d6e0349a66
moneyandfame7 <davidoo1234e@gmail.com>
2023-05-01 18:10:15 +0300

    commit

--- src/infrastructure/dto/authorization/authorization.dto.ts
+++ src/infrastructure/dto/authorization/authorization.dto.ts
@@ -1,31 +1,31 @@
-import {ApiProperty} from '@nestjs/swagger'
-import { IsEmail, MaxLength, MinLength} from 'class-validator'
-import {Type} from 'class-transformer'
+import { ApiProperty } from '@nestjs/swagger'
+import { IsEmail, MaxLength, MinLength } from 'class-validator'
+import { Type } from 'class-transformer'
 
 export class SignupDto {
-    @ApiProperty({example: 'user@gmail.com', description: 'The email of the user.'})
-    @IsEmail()
-    public readonly email: string
+  @ApiProperty({ example: 'user@gmail.com', description: 'The email of the user.' })
+  @IsEmail()
+  public readonly email: string
 
-    @ApiProperty({
-        example: '123user123',
-        description: 'The password of the user.'
-    })
-    @MinLength(8)
-    @MaxLength(20)
-    @Type(() => String)
-    public readonly password: string
+  @ApiProperty({
+    example: '123usesr123',
+    description: 'The password of the user.'
+  })
+  @MinLength(8)
+  @MaxLength(20)
+  @Type(() => String)
+  public readonly password: string
 }
 
 export class LoginDto {
-    @IsEmail()
-    public readonly email: string
+  @IsEmail()
+  public readonly email: string
 
-    @Type(() => String)
-    public readonly password: string;
+  @Type(() => String)
+  public readonly password: string
 }
 
 export class ForgotDto {
-    @IsEmail()
-    public readonly email: string
+  @IsEmail()
+  public readonly email: string
 }
